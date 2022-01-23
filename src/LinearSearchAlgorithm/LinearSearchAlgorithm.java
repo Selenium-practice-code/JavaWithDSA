d28bebd8c1e0bfe6d20daa22b2024bfa77a1d738
sumitagnihotri <practiceselenium7@gmail.com>
2022-01-24 00:07:26 +0530

    LinerSearhAlgorithmExamples

---
+++ src/LinearSearchAlgorithm/LinearSearchAlgorithm.java
@@ -0,0 +1,11 @@
+package LinearSearchAlgorithm;
+
+public class LinearSearchAlgorithm {
+    /**
+     * * Linear search algorithm says start searching from
+     * * First element till you find the element you're looking for.
+     */
+    public static void main(String[] args) {
+
+    }
+}
