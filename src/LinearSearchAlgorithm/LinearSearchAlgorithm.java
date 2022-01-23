c8a0345d8286ddcdf1e5e02d57b6d2d8097906b3
sumitagnihotri <practiceselenium7@gmail.com>
2022-01-24 00:07:39 +0530

    LinerSearhAlgorithmExamples

--- src/LinearSearchAlgorithm/LinearSearchAlgorithm.java
+++ src/LinearSearchAlgorithm/LinearSearchAlgorithm.java
@@ -7,5 +7,60 @@ public class LinearSearchAlgorithm {
      */
     public static void main(String[] args) {
 
+        int[] nums = {1, 2, 18, 20, -19, 10, -1, 89, 78};
+        int target = -1;
+        int result = linearSearch(nums, target);
+        int ans = linearSearchAlgo(nums, target);
+        System.out.println("Index is : " + result);
+        System.out.println("Element is: " + ans);
+
     }
+
+    // todo: Search in the array-> rerun the index if item found 
+    // todo: otherwise if item not found return -1
+    public static int linearSearch(int[] arr, int target) {
+
+        //* check if element is in array
+        if (arr.length == 0) {
+            return -1; //! if element not found in the array it returns -1
+        }
+
+        //* run for loop
+        for (int index = 0; index < arr.length; index++) {
+
+            //* check for element at every index if it is = target
+            int element = arr[index];
+            if (element == target) {
+                return index;
+            }
+        }
+
+        //* this line will execute if none of the rerun statements above have executed
+        return -1; //! hence, target not found
+    }
+
+    // todo: Search the target and return the element
+    public static int linearSearchAlgo(int[] arr, int target) {
+
+        //* check if element is in array
+        if (arr.length == 0) {
+            return -1; //! if element not found in the array it returns -1
+        }
+
+        //* run for loop
+        for (int element : arr) {
+            //* check for element at every index if it is = target
+            if (element == target) {
+                return element;
+            }
+        }
+
+        //* this line will execute if none of the rerun statements above have executed
+        return -1; //! hence, target not found
+    }
+
 }
+
+
+
+
