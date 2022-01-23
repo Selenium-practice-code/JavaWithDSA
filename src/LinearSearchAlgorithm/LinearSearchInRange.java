6b7a251dcdcb758e2d7983ebe8f9e352b9c83b41
sumitagnihotri <practiceselenium7@gmail.com>
2022-01-24 01:55:41 +0530

    Add LinerSearh In Range Example

--- src/LinearSearchAlgorithm/LinearSearchInRange.java
+++ src/LinearSearchAlgorithm/LinearSearchInRange.java
@@ -6,13 +6,15 @@ public class LinearSearchInRange {
 
         //? Search for the target element in the range of given Index
         int[] nums = {1, 2, 18, 20, -19, 10, 12, 89, 78};
-        int target = 20;
-        System.out.println("Index is : " + linearSearchInRange(nums, target, 0, 7));
+        int target = 89;
+        int startIndex = 6;
+        int endIndex = 8;
+        System.out.println("Index is : " + linearSearchInRange(nums, target, startIndex, endIndex));
 
     }
 
     // todo: Search the target form given range and return the element
-    public static int linearSearchInRange(int[] arr, int target, int start, int end) {
+    public static int linearSearchInRange(int[] arr, int target, int startIndex, int endIndex) {
 
         //* check if element is in array
         if (arr.length == 0) {
@@ -20,7 +22,7 @@ public class LinearSearchInRange {
         }
 
         //* run for loop from index start till end
-        for (int index = start; index <= end; index++) {
+        for (int index = startIndex; index <= endIndex; index++) {
 
             //* check for element at every index if it is = target
             int element = arr[index];
